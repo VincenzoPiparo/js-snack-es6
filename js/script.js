4a7efdc61fc1918d551a921e42d95acc4a4b6a78
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-29 21:52:03 +0100

    END SNACK 2

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// Snack 1
-// Dato l'array di nomi:
-// const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
-// e dati due numeri min e max (min più piccolo di max).
-// Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
-// Eseguiamo questo esercizio prima con forEach e poi con filter.
