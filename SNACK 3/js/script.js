55f8c908462dc5174ab4b67a45cd372acba8ced0
VincenzoPiparo <v.pipar.97@gmai.com>
2021-12-07 14:59:41 +0100

    END SNACK BIKE

--- SNACK 3/js/script.js
+++ SNACK 3/js/script.js
@@ -2,7 +2,7 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
 
-// 1 [x] Creare una costante:
+// 1 [x] Creation a const:
 
 const bikes =
 [
@@ -21,3 +21,32 @@ const bikes =
         weight: 6
     }
 ];
+
+// 2 [x] Use the first element by array
+
+let bikeLight = bikes [0];
+
+// 3 [] With cycle for 
+
+for(let i = 0; i < bikes.length; i++) {
+
+    const thisBike = bikes[i];
+
+    // we scroll through each bicycle, when the lighter one comes out we save it in bikeLight
+    // Use the IF
+    if(thisBike.weight < bikeLight.weight) {
+        bikeLight = thisBike;
+    }
+}
+
+console.log(bikeLight);
+
+// We display the lightest bike on the screen
+
+const {name, weight} = bikeLight;
+
+const bikesDOM = `
+<p> The Bike more light is ${name} your weight is ${weight} kg </p>
+`;
+
+document.getElementById('bicycle').innerHTML = bikesDOM
